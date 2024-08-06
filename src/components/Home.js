1aa637f1094f4b9b596ab2391b17583c1ff82672
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-06 11:45:33 +0300

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
+import "./home.css"
+
 
 function Home() {
+
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+  <h1 id="h1">Your {name} is a Web Developer from Your {city}</h1>
+  </div>;
 }
 
 export default Home;
